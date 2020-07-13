4b8a3fb0484e55a3aa9725c3b1dd84597ea35abc
liang.shujin <liang.shujin@harqom.com>
2020-07-13 15:04:45 +0800

    路由模块化

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
 import Vue from 'vue';
+// import App from './App';
+import App from './App.vue';
+
 //引入Element-UI
 import 'element-ui/lib/theme-chalk/index.css';
 import ElementUI from 'element-ui';
-import App from './App';
-import VueRouter from "vue-router";
+import router from './router/router.js';
 import VueResource from 'vue-resource';
+
+
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-Vue.use(VueRouter);
 Vue.use(VueResource);
 
 //封装Cookie操作
@@ -22,36 +25,9 @@ Vue.prototype.$httpAddress = "http://192.168.19.20:8084";
 //同一内容区高度
 Vue.prototype.$height = window.innerHeight - 160 +'px';
 
-//创建组件
-import Login from "../src/components/Login.vue";
-import Index from "../src/components/Index.vue";
-import UpdataUserPsw from "../src/components/UpdataUserPsw.vue";
-import UserList from "../src/components/UserList.vue";
-import CaseList from "../src/components/CaseList.vue";
-import SystemLog from "../src/components/SystemLog.vue";
-import Location from "../src/components/Location.vue";
-import DepartList from "./components/DepartList";
 
 
-//配置路由
-const routes = [
-  { path: '*',  redirect: '/login' },
-  { path: '/login', component: Login },
-  { path: '/index', component: Index ,children:[
-      {path: '', component: UserList },
-      {path: '/upDataUserPsw', component: UpdataUserPsw },
-      {path: '/departList', component: DepartList },
-      {path: '/userList', component: UserList },
-      {path: '/caseList', component: CaseList },
-      {path: '/systemLog', component: SystemLog },
-      {path: '/location', component: Location },
-    ]}
-]
 
-//实例化路由
-const router = new VueRouter({
-  routes // (缩写) 相当于 routes: routes
-})
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/router/router.js
@@ -0,0 +1,35 @@
+// import VueRouter from "vue-router";
+import Vue from 'vue';
+import VueRouter from "vue-router";
+Vue.use(VueRouter);
+
+//路由配置
+import Login from "../components/Login.vue";
+import Index from "../components/Index.vue";
+import UpdataUserPsw from "../components/UpdataUserPsw.vue";
+import UserList from "../components/UserList.vue";
+import CaseList from "../components/CaseList.vue";
+import SystemLog from "../components/SystemLog.vue";
+import Location from "../components/Location.vue";
+import DepartList from "../components/DepartList.vue";
+
+const routes = [
+    { path: '*',  redirect: '/login' },
+    { path: '/login', component: Login },
+    { path: '/index', component: Index ,children:[
+        {path: '', component: UserList },
+        {path: '/upDataUserPsw', component: UpdataUserPsw },
+        {path: '/departList', component: DepartList },
+        {path: '/userList', component: UserList },
+        {path: '/caseList', component: CaseList },
+        {path: '/systemLog', component: SystemLog },
+        {path: '/location', component: Location },
+    ]}
+]
+
+const router = new VueRouter({
+    // mode: 'history',
+    routes // (缩写) 相当于 routes: routes
+})
+
+export default router;
